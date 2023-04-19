1e6e18b5e97f2552e1c7a295ac29e35099f8e1fb
Rafael Guimaraes <rafalalai21@gmail.com>
2023-04-19 15:59:06 -0300

    Exercicio feito até o 8 da seção 5

--- Curso_JS/Sprint1/script1.js
+++ Curso_JS/Sprint1/script1.js
@@ -12,6 +12,7 @@ function verifica_num(num_certo){
     console.log("Hello World!");
     var numcerto = 100;
 
+    // Seção 3 do curso, contém exercícios básicos sobre tipos de dados.
 
 function ex1(){
     console.log("duplas");
@@ -36,4 +37,35 @@ function ex5(){
     console.log(1*3*"nan");
 }
 
-ex5();
+// Seção 5 do curso de JS da Udemy
+function ex1S5(){
+    let teste1 = True, teste2 = "string" , teste3 = 1
+}
+
+
+// Função faz a verificação se o usuário é maior de idade, a partir de uma idade digitada
+function ex2S5(){
+    let idade = prompt("Digite a sua idade para entrada na balada:")
+    if( idade >= 18){
+        alert("Seja bem-vindo!!");
+    }
+    else{
+        alert("Probido entrada de menores de 18 anos");
+    }
+}
+
+
+function ex3S5(){
+    let listanomes = ['Rafael','Luis','Ruan'];
+
+    let resp = prompt("Digite o nome para entrada. (LISTA DE ENTRADA)");
+    for(let i = 0; i < listanomes.length ; i++){
+        if(resp == listanomes[i]){
+            alert("Permitido a entrada");
+        }
+        else{
+            alert("Entrada BARRADA. Motivo: NOME NÃO ESTÁ NA LISTA ");
+        }
+
+    }
+}
