5327e946ea541a8aca3a36f0bf2a8bed59454ab8
Rafael Guimaraes <rafalalai21@gmail.com>
2023-04-27 13:56:14 -0300

    Fim da seção 15

--- Curso_JS/Sprint2/Secao13_Async/src/Secao13.js
+++ Curso_JS/Sprint2/Secao13_Async/src/Secao13.js
@@ -11,3 +11,43 @@ let p = Promise.resolve(5)
 console.log('outros códigos')
 //console.log(p)
 p.then((value) => {console.log(`O valor é ${value}`)})
+
+async function somar(a,b){
+    return a+b
+}
+console.log("Teste Async")
+somar(2,2).then(value => console.log(value))
+
+function somaComDelay(a,b){
+    return new Promise(resolve => {
+        setTimeout(function() {
+            resolve(a+b)
+        }, 4000)
+    })
+}
+
+async function resSoma(a,b,c){
+    let x = somaComDelay(a,b)
+    let y = c
+
+    return await x + y
+}
+
+console.log(resSoma(1,2,3).then(value => console.log(value)))
+
+function* criadorID(){
+    let id = 0
+    while(true){
+        yield id++
+    }
+        
+}
+
+let criaId = criadorID()
+console.log(criaId.next().value)
+console.log(criaId.next().value)
+console.log(criaId.next().value)
+console.log(criaId.next().value)
+console.log(criaId.next().value)
+
+console.log("acabou")
