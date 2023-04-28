aaa2167ed048ad584e16ab1a1cede3b24057fada
Rafael Guimaraes <rafalalai21@gmail.com>
2023-04-28 17:20:32 -0300

    Jogo funcionando pra jogar 1v1

--- Curso_JS/Jogo_da_Velha/js/scripts.js
+++ Curso_JS/Jogo_da_Velha/js/scripts.js
@@ -20,7 +20,7 @@ for(let i =0; i < boxes.length; i++) {
             player1++
         else
             player2++
-            
+
         // verifica se já tem uma jogada no bloco
         if(this.childNodes.length == 0){
             let cloneEl = el.cloneNode(true)
@@ -28,6 +28,8 @@ for(let i =0; i < boxes.length; i++) {
             // eu n coloquei if aqui n
         }
 
+        // checa ganhador
+        checkWinCondition()
     });
 
     
@@ -42,4 +44,161 @@ function verificaJogador(player1,player2){
         el = o
     }
     return el
+}
+
+function checkWinCondition(){
+     let b1 = document.getElementById("block-1")
+     let b2 = document.getElementById("block-2")   
+     let b3 = document.getElementById("block-3")
+     let b4 = document.getElementById("block-4")
+     let b5 = document.getElementById("block-5")
+     let b6 = document.getElementById("block-6")
+     let b7 = document.getElementById("block-7")
+     let b8 = document.getElementById("block-8")
+     let b9 = document.getElementById("block-9")
+
+     //horizontal
+     if(b1.childNodes.length > 0 && b2.childNodes.length > 0 && b3.childNodes.length > 0){
+        let b1Child = b1.childNodes[0].className;
+        let b2Child = b2.childNodes[0].className;
+        let b3Child = b3.childNodes[0].className;
+        if(b1Child == 'x' && b2Child == 'x' && b3Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b1Child == 'o' && b2Child == 'o' && b3Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b4.childNodes.length > 0 && b5.childNodes.length > 0 && b6.childNodes.length > 0){
+        let b4Child = b4.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        if(b4Child == 'x' && b5Child == 'x' && b6Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b4Child == 'o' && b5Child == 'o' && b6Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b7.childNodes.length > 0 && b8.childNodes.length > 0 && b9.childNodes.length > 0){
+        let b7Child = b7.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+        if(b7Child == 'x' && b8Child == 'x' && b9Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b7Child == 'o' && b8Child == 'o' && b9Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b1.childNodes.length > 0 && b5.childNodes.length > 0 && b9.childNodes.length > 0){
+        let b1Child = b1.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+        if(b1Child == 'x' && b5Child == 'x' && b9Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b1Child == 'o' && b5Child == 'o' && b9Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b3.childNodes.length > 0 && b5.childNodes.length > 0 && b7.childNodes.length > 0){
+
+        let b3Child = b3.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+
+        if(b3Child == 'x' && b5Child == 'x' && b7Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b3Child == 'o' && b5Child == 'o' && b7Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b1.childNodes.length > 0 && b4.childNodes.length > 0 && b7.childNodes.length > 0){
+        let b1Child = b1.childNodes[0].className;
+        let b4Child = b4.childNodes[0].className;
+        let b7Child = b7.childNodes[0].className;
+        if(b1Child == 'x' && b4Child == 'x' && b7Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b1Child == 'o' && b4Child == 'o' && b7Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b2.childNodes.length > 0 && b5.childNodes.length > 0 && b8.childNodes.length > 0){
+        let b2Child = b2.childNodes[0].className;
+        let b5Child = b5.childNodes[0].className;
+        let b8Child = b8.childNodes[0].className;
+        if(b2Child == 'x' && b5Child == 'x' && b8Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b2Child == 'o' && b5Child == 'o' && b8Child == 'o'){
+            declareWinner('o')
+        }
+     }
+
+     else if(b3.childNodes.length > 0 && b6.childNodes.length > 0 && b9.childNodes.length > 0){
+        let b3Child = b3.childNodes[0].className;
+        let b6Child = b6.childNodes[0].className;
+        let b9Child = b9.childNodes[0].className;
+        if(b3Child == 'x' && b6Child == 'x' && b9Child == 'x'){
+            console.log('win')
+            declareWinner('x')
+        } else if(b3Child == 'o' && b6Child == 'o' && b9Child == 'o'){
+            declareWinner('o')
+
+        }
+     }
+     // deu velha
+     let counter = 0;
+     for(let i = 0; i < boxes.length; i++) {
+        if(boxes[i].childNodes[0] != undefined)
+            counter++
+     }
+
+     if (counter == 9)
+        declareWinner()
+
+}
+//limpa o jogo, declarando vencedor e att placar
+function declareWinner(winner){
+    let scoreboardX = document.querySelector("#scoreboard-1")
+    let scoreboardY = document.querySelector("#scoreboard-2")
+    let msg =''
+    if(winner == 'x'){
+    scoreboardX.textContent = parseInt(scoreboardX.textContent) + 1
+    msg = "O jogador 1 venceu!!"
+    } else if(winner == 'o'){
+        scoreboardY.textContent = parseInt(scoreboardY.textContent) + 1
+        msg = "O jogador 2 venceu!!"
+    } else
+    msg = 'Deu velha!!'
+
+    //exibe msg
+    messageText.innerHTML = msg
+    messageContainer.classList.remove("hide")
+
+// esconde msg
+    setTimeout(function(){
+        messageContainer.classList.add("hide")
+    }, 3000)
+
+    // zera
+
+    player1 = 0
+    player2 = 0
+
+    //remove x e o
+    let boxesRemove = document.querySelectorAll(".box div")
+    for(let i = 0; i < boxesRemove.length; i++) {
+        boxesRemove[i].parentNode.removeChild(boxesRemove[i])
+    }
+
 }
